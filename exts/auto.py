0fc17d6fed917733a36915bad442664a522df7af
LLOOOOTT <gm3294@naver.com>
2021-02-05 14:53:12 +0900

    Fix to get member No delete feature todo;

--- exts/auto.py
+++ exts/auto.py
@@ -15,10 +15,24 @@ class Automatic(commands.Cog, name="자동 응답기"):
         if not rows:
             await c.execute(f"INSERT INTO cc(word, reply, lock, author) VALUES('{word}', '{reply}', 'false', '{ctx.author.id}')")
             await o.commit()
-            await ctx.send(f"")
+            await o.close()
+            await ctx.send(f"<:cs_yes:659355468715786262> {ctx.author.mention} - `{word}` 단어를 추가했어요.")
         else:
+            await o.close()
             raise commands.BadArgument
-        await o.close()
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            o = await aiosqlite.connect("Shark1ight.sqlite")
+            c = await o.cursor()
+            name = ctx.message.content.split(" ")[1]
+            await c.execute(f"SELECT * FROM cc WHERE word = '{name}'")
+            rows = await c.fetchall()
+            if rows:
+                user = self.bot.get_user(int(rows[0][3]))
+                await ctx.send(f"{rows[0][1]}\n \nMade by `{user}`")
+            await o.close()
 
 def setup(bot):
     bot.add_cog(Automatic(bot))
